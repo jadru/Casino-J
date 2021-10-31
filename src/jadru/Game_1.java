edebedd13ef05c45742d9b28b72805d4218c084f
Younggun Park <ygpark0922@gmail.com>
2021-11-01 00:29:41 +0900

    아 디비 뭐냐

--- src/jadru/Game_1.java
+++ src/jadru/Game_1.java
@@ -9,51 +9,85 @@ import static jadru.ShuffleCard.makeCard;
 import static jadru.ShuffleCard.pickCards;
 
 public class Game_1 extends JFrame {
-    private GamePanel game = new GamePanel();
-    Game_1(){
+    private GamePanel main = new GamePanel("");
+    private static final int PADDING = 20;
+    Game_1() {
         setTitle("게임1");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setContentPane(game);
-        setSize(1080,720);
-        new GameCore();
+        setContentPane(main);
+        setLayout(null);
+        setSize(1280, 740);
+        setLocationRelativeTo(null);
+        setResizable(false);
+
+        String[][] cards = makeCard();
+        int[] user_card = pickCards(7);
+        int[] com_card = pickCards(7);
+
+        JPanel com_profile = new JPanel(new CardLayout());
+        JPanel user_profile = new JPanel(new CardLayout());
+        JPanel com_cards = new JPanel(null);
+        JPanel user_cards = new JPanel(new FlowLayout(FlowLayout.CENTER, PADDING, PADDING));
+
+        com_profile.setSize(160, 160);
+        user_profile.setSize(160, 160);
+        com_cards.setSize(1280, 200);
+        user_cards.setSize(1280, 200);
+
+        JLabel com_profile_img = new JLabel("문자열", new ImageIcon("asset/icons8-bot-96.png"), SwingConstants.CENTER);
+        com_profile.add(com_profile_img);
+        JLabel user_profile_img = new JLabel("문자열", new ImageIcon("asset/icons8-text-account-96.png"), SwingConstants.CENTER);
+        user_profile.add(user_profile_img);
+
+        com_profile.setLocation(0, 0);
+        user_profile.setLocation(1120, 560);
+        com_cards.setLocation(0, 160);
+        user_cards.setLocation(0, 360);
+
+        com_cards.setBackground(Color.black);
+        user_cards.setBackground(Color.darkGray);
+
+        add(com_profile);
+        add(user_profile);
+        add(com_cards);
+        add(user_cards);
+
+        for (int i = 0; i < 7; i++) {
+            JButton btn = new JButton();
+            btn.setText(cards[user_card[i] / 13][user_card[i] % 13]);
+            btn.addActionListener(new cardActionListener());
+            btn.setBackground(Color.WHITE);
+            btn.setPreferredSize(new Dimension(120, 160));
+            btn.setMargin(new Insets(0, 0, 0, 0));
+            btn.setFont(new Font(null, Font.BOLD, 30));
+            user_cards.add(btn);
+        }
+
         setVisible(true);
+        com_profile.setVisible(true);
+        user_profile.setVisible(true);
+        com_cards.setVisible(true);
+        user_cards.setVisible(true);
+    }
+
+    class cardActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JButton btn = (JButton) e.getSource();
+
+        }
     }
 
     class GamePanel extends JPanel {
-        private ImageIcon icon = new ImageIcon("C:\\Users\\SeongByeongseok\\Desktop\\img.png");
+        String url = null;
+        GamePanel(String url) {
+            this.url = url;
+        }
+        private ImageIcon icon = new ImageIcon(url);
         private Image img = icon.getImage();
         public void paintComponent(Graphics g){
             super.paintComponent(g);
             g.drawImage(img, 0,0,getWidth(),getHeight(),this);
-        }
-    }
-
-    class GameCore {
-        class cardListener implements ActionListener {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JButton btn = (JButton)e.getSource();
-
-            }
-        };
-        GameCore(){
-            String[][] cards = makeCard();
-            int[] user_card = pickCards(7);
-            int[] com_card = pickCards(7);
-            JButton start = new JButton("시작하기");
-            add(start);
-            start.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    for(int i = 0; i < 7; i++){
-                        JButton btn = new JButton();
-                        btn.setText(cards[user_card[i]/13][user_card[i]%13]);
-                        btn.addActionListener(new cardListener());
-                        add(btn);
-                    }
-                }
-            });
-
         }
 
     }
