a25ded53cbf9f675846c72f005f579ed956b7225
BSS <qudtjr4218@gmail.com>
2021-11-29 21:13:22 +0900

    button으로 교체

--- src/screen/ProfileScreen.java
+++ src/screen/ProfileScreen.java
@@ -9,7 +9,7 @@ import java.awt.*;
 public class ProfileScreen extends JFrame{
     private MyPanel panel = new MyPanel();
     private Image card1_back,card2_back,card3_back,card4_back,card5_back;
-    private Image card1_front,card2_front,card3_front,card4_front,card5_front;
+
     public boolean checkFrontBack = true;
 	ProfileScreen(String id){
         super("프로필"); //타이틀
@@ -25,27 +25,7 @@ public class ProfileScreen extends JFrame{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setContentPane(panel);
         setLayout(null);
-        JPanel jp = new JPanel(){
-            @Override
-            public void paint(Graphics g) {
-                if (checkFrontBack == true) {
-                    g.drawImage(card1_back, 0, 0, 130, 200, this);
-                    g.drawImage(card2_back, 150, 0, 130, 200, this);
-                    g.drawImage(card3_back, 300, 0, 130, 200, this);
-                    g.drawImage(card4_back, 0, 250, 130, 200, this);
-                    g.drawImage(card5_back, 150, 250, 130, 200, this);
-                }
-                else{
-                    g.drawImage(card1_front, 0, 0, 130, 200, this);
-                    g.drawImage(card2_front, 150, 0, 130, 200, this);
-                    g.drawImage(card3_front, 300, 0, 130, 200, this);
-                    g.drawImage(card4_front, 0, 250, 130, 200, this);
-                    g.drawImage(card5_front, 150, 250, 130, 200, this);
-                }
-            }
-        };
-        jp.setBackground(Color.white);
-        jp.setBounds(690,150,450,450);
+
         JButton mainButton = new JButton("메인");
         mainButton.setBounds(30,30,70,70);
 
@@ -53,19 +33,8 @@ public class ProfileScreen extends JFrame{
         JLabel[] gridTitle = new JLabel[6];
         JLabel[] gridElement = new JLabel[6];
         JLabel CardText = new JLabel("보유중인 카드");
-        JButton CardFrontBack = new JButton("카드 앞/뒷면 보기");
+        JButton CardBt[] = new JButton[5];
 
-        CardFrontBack.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if(checkFrontBack == true)
-                    checkFrontBack = false;
-                else
-                    checkFrontBack = true;
-                revalidate();
-                repaint();
-            }
-        });
 
 
 
@@ -75,28 +44,27 @@ public class ProfileScreen extends JFrame{
         ImageIcon card4_backside = new ImageIcon("asset/card_back_4.png");
         ImageIcon card5_backside = new ImageIcon("asset/card_back_0.png");
 
-        ImageIcon card1_frontside = new ImageIcon("asset/card1_front_side.jpeg");
-        ImageIcon card2_frontside = new ImageIcon("asset/card2_front_side.jpeg");
-        ImageIcon card3_frontside = new ImageIcon("asset/card3_front_side.jpeg");
-        ImageIcon card4_frontside = new ImageIcon("asset/card4_front_side.jpeg");
-        ImageIcon card5_frontside = new ImageIcon("asset/card5_front_side.jpeg");
-
         card1_back = card1_backside.getImage();
         card2_back = card2_backside.getImage();
         card3_back = card3_backside.getImage();
         card4_back = card4_backside.getImage();
         card5_back = card5_backside.getImage();
 
-        card1_front = card1_frontside.getImage();
-        card2_front = card2_frontside.getImage();
-        card3_front = card3_frontside.getImage();
-        card4_front = card4_frontside.getImage();
-        card5_front = card5_frontside.getImage();
+        CardBt[0] = new JButton(card1_backside);
+        CardBt[1] = new JButton(card2_backside);
+        CardBt[2] = new JButton(card3_backside);
+        CardBt[3] = new JButton(card4_backside);
+        CardBt[4] = new JButton(card5_backside);
 
+
+        CardBt[0].setBounds(690,150,130,160);
+        CardBt[1].setBounds(840,150,130,160);
+        CardBt[2].setBounds(990,150,130,160);
+        CardBt[3].setBounds(690,350,130,160);
+        CardBt[4].setBounds(840,350,130,160);
 
 
         CardText.setFont(new Font("Gothic",Font.BOLD,25));
-        CardFrontBack.setFont(new Font("Gothic",Font.BOLD,25));
 
         for(int i = 0; i < 6; i++) {
             gridTitle[i] = new JLabel(title[i]);
@@ -120,16 +88,14 @@ public class ProfileScreen extends JFrame{
             add(gridElement[i]);
         }
         CardText.setBounds(690, 50,150,100);
-        CardFrontBack.setBounds(850, 75,200,50);
-        add(jp);
+
+
 
         add(CardText);
-        add(CardFrontBack);
         add(mainButton);
+        for(int i =0; i< 5; i++)
+            add(CardBt[i]);
         setSize(1280,720);
-
-
-        
 
         Dimension frameSize = getSize();
         Dimension windowSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -137,7 +103,7 @@ public class ProfileScreen extends JFrame{
                 (windowSize.height - frameSize.height) / 2); //화면 중앙에 띄우기
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
-        
+
         mainButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
