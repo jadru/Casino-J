52de0a6e215730cadb3ce19ecb06bec89d5569ad
Younggun Park <ygpark0922@gmail.com>
2021-12-04 19:46:50 +0900

    Update LoginScreen.java

--- src/screen/ui/LoginScreen.java
+++ src/screen/ui/LoginScreen.java
@@ -5,28 +5,23 @@ import support.SQLiteManager;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
 
 public class LoginScreen extends GlobalGUI {
     public LoginScreen() {
         super("Casino-J에 로그인", "src/asset/bg/LoginImg.png");
 
-        JTextField ID = new JTextField();
-        JPasswordField PW = new JPasswordField();
+        JTextField ID = new JTextField("ID");
+        JPasswordField PW = new JPasswordField("PASS");
         ID.setBackground(Color.BLACK);
         PW.setBackground(Color.BLACK);
-        ID.setFont(casinoFont(35));
-        PW.setFont(casinoFont(35));
         ID.setForeground(Color.WHITE);
         PW.setForeground(Color.WHITE);
-        ID.setFocusable(true);
         ID.requestFocus();
         ID.grabFocus();
-        JLabel id_label = new JLabel("ID : ");
-        JLabel pw_label = new JLabel("PW : ");
+        ID.setBounds(460, 400, 250, 50);
+        PW.setBounds(770, 400, 250, 50);
+
         JLabel gameList[] = new JLabel[3];
 
         String str[] = {"로그인", "회원가입"};
@@ -35,7 +30,6 @@ public class LoginScreen extends GlobalGUI {
         ImageIcon bt0 = new ImageIcon("src/asset/btn/LogInButton.png");
         ImageIcon bt1 = new ImageIcon("src/asset/btn/SignUpButton.png");
         ImageIcon list1 = new ImageIcon("src/asset/ui/login_gamelist_1.png");
-        ImageIcon list2 = new ImageIcon("src/asset/ui/login_gamelist_2.png");
         ImageIcon list3 = new ImageIcon("src/asset/ui/login_gamelist_3.png");
         ImageIcon Doge = new ImageIcon("src/asset/DOGE.png");
         JLabel Coin = new JLabel(Doge);
@@ -47,31 +41,42 @@ public class LoginScreen extends GlobalGUI {
         }
         bt[0].setBounds(450, 450, 305, 100);
         bt[1].setBounds(760, 450, 305, 100);
+
         bt[0].setBorderPainted(false);
         bt[0].setContentAreaFilled(false);
+
         bt[1].setBorderPainted(false);
         bt[1].setContentAreaFilled(false);
-        ID.setBounds(460, 400, 250, 50);
-        PW.setBounds(770, 400, 250, 50);
-        id_label.setBounds(570, 530, 130, 30);
-        pw_label.setBounds(570, 570, 130, 30);
+
         Coin.setBounds(800, 160, 250, 250);
         gameList[0] = new JLabel(list1);
-        gameList[1] = new JLabel(list2);
         gameList[2] = new JLabel(list3);
 
         gameList[0].setBounds(200, 160, 400, 80);
-        gameList[1].setBounds(200, 230, 400, 80);
         gameList[2].setBounds(200, 310, 400, 80);
         for (int i = 0; i < 2; i++)
             add(bt[i]);
         for (int i = 0; i < 3; i++)
-            add(gameList[i]);
+            if(i != 1) add(gameList[i]);
         add(ID);
         add(PW);
-        add(id_label);
-        add(pw_label);
         add(Coin);
+
+        ID.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if(ID.getText().equals("ID"))
+                    ID.setText("");
+            }
+        });
+
+        PW.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if(ID.getText().equals("PASS"))
+                    ID.setText("");
+            }
+        });
 
         PW.addKeyListener(new KeyAdapter() {
             @Override
